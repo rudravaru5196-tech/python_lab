72c3f74992dcd7034afec7186e2bc0742764b3ab
rudravaru5196-tech <rudravaru5196@gmail.com>
2026-04-10 07:58:58 +0530

    Create cond_st.py

---
+++ cond_st.py
@@ -0,0 +1,54 @@
+#age 
+
+"""age = int(input("enter your age :"))
+if age>=18:
+    print("yes you are eligible.")
+else:
+    print("you are not eligible.")"""
+
+#marks
+
+"""marks=int(input("enter your marks:"))
+if marks>=90:
+    print("you get A grade")
+elif marks>=80:
+    print("you get B grade")
+elif marks>=50:
+    print("you get C grade")
+else :
+    print("you are pass")"""
+
+#vote
+
+"""age = int(input("enter your age :"))
+if age>=18:
+    print("yes you are eligible for vote.")
+else:
+    print("you are not eligible for vote.")"""
+
+#odd & even
+
+"""number =int(input("enter number:"))
+if number %2==0:
+    print("the number is even.")
+else:
+    print("tne number is odd.")"""
+
+
+"""#positive & negetive
+
+num=int(input("enter number: "))
+if num>0:
+    print("positive number")
+elif num<0:
+    print("negitive number.")
+else:
+    print("zero")"""
+
+# licence
+
+age = int(input("enter your age :"))
+if age>=18:
+    print("yes you are eligible for licence.")
+else:
+    print("you are not eligible for licence.")
